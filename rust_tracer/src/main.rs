fa498f0ac58f1121bbf7d530bb8325877360b6c2
João Xavier <fastjonh@gmail.com>
2023-06-02 11:58:21 -0300

    refac on Vector struct to shorten names
    refac variables names

--- rust_tracer/src/main.rs
+++ rust_tracer/src/main.rs
@@ -1,9 +1,9 @@
 use rand::Rng;
-use vector::Vector3;
+use vector::Vec3;
 
 mod vector;
 
-const GREY_COLOR: Vector3 = Vector3 {
+const GREY_COLOR: Vec3 = Vec3 {
     x: 13.,
     y: 13.,
     z: 13.,
@@ -21,17 +21,14 @@ fn main() {
 }
 
 fn render_image(width: u32, height: u32) {
-    let camera_direction = Vector3::new(-6., -16., 0.).normalize();
+    let camera_direction = Vec3::new(-6., -16., 0.).norm();
 
-    let camera_right = Vector3::new(0., 0., 1.)
-        .cross_product(camera_direction)
-        .normalize()
+    let camera_right = Vec3::new(0., 0., 1.)
+        .cross(camera_direction)
+        .norm()
         .scale(0.002);
 
-    let camera_up = camera_direction
-        .cross_product(camera_right)
-        .normalize()
-        .scale(0.002);
+    let camera_up = camera_direction.cross(camera_right).norm().scale(0.002);
 
     let camera_position = camera_right
         .add(camera_up)
@@ -54,9 +51,9 @@ fn render_image(width: u32, height: u32) {
                     .add(camera_position)
                     .scale(16.)
                     .add(offset.scale(-1.))
-                    .normalize();
+                    .norm();
 
-                let ray_direction = Vector3::new(17., 16., 8.).add(offset);
+                let ray_direction = Vec3::new(17., 16., 8.).add(offset);
 
                 let sampled_pixel_color = sample_pixel_color(ray_direction, ray_origin);
                 pixel_color = sampled_pixel_color.scale(3.5).add(pixel_color);
@@ -74,15 +71,15 @@ fn random() -> f32 {
     rand::thread_rng().gen()
 }
 
-fn trace(o: Vector3, d: Vector3, distance: &mut f32, surface_normal: &mut Vector3) -> i32 {
+fn trace(direction: Vec3, origin: Vec3, distance: &mut f32, surface_normal: &mut Vec3) -> i32 {
     *distance = 1e9;
 
     let mut material = 0;
-    let intersection_distance = -o.z / d.z;
+    let intersection_distance = -direction.z / origin.z;
 
     if 0.01 < intersection_distance {
         *distance = intersection_distance;
-        *surface_normal = Vector3::new(0., 0., 1.);
+        *surface_normal = Vec3::new(0., 0., 1.);
         material = 1;
     }
 
@@ -90,17 +87,17 @@ fn trace(o: Vector3, d: Vector3, distance: &mut f32, surface_normal: &mut Vector
         for j in (0..9i32).rev() {
             let render_sphere = SPHERES_LOCATION[j as usize] & 1 << k != 0;
             if render_sphere {
-                let p = o.add(Vector3::new(-k as f32, 0., -j as f32 - 4.));
+                let p = direction.add(Vec3::new(-k as f32, 0., -j as f32 - 4.));
 
-                let b = p.dot_product(d);
-                let c = p.dot_product(p) - 1.;
-                let q = b * b - c;
+                let projection = p.dot(origin);
+                let quadratic_coefficient = p.dot(p) - 1.;
+                let discriminant = projection * projection - quadratic_coefficient;
 
-                if q > 0. {
-                    let s = -b - q.sqrt();
+                if discriminant > 0. {
+                    let s = -projection - discriminant.sqrt();
                     if s < *distance && s > 0.01 {
                         *distance = s;
-                        *surface_normal = p.add(d.scale(*distance)).normalize();
+                        *surface_normal = p.add(origin.scale(*distance)).norm();
                         material = 2;
                     }
                 }
@@ -111,27 +108,27 @@ fn trace(o: Vector3, d: Vector3, distance: &mut f32, surface_normal: &mut Vector
     material //material intersection 0 = None, 1 = plane, 2 = sphere
 }
 
-fn sample_pixel_color(o: Vector3, d: Vector3) -> Vector3 {
+fn sample_pixel_color(direction: Vec3, origin: Vec3) -> Vec3 {
     let mut distance = 0.;
 
-    let mut surface_normal = Vector3::zero();
+    let mut surface_normal = Vec3::zero();
 
-    let material = trace(o, d, &mut distance, &mut surface_normal);
+    let material = trace(direction, origin, &mut distance, &mut surface_normal);
 
     if material == 0 {
-        let a1 = 1. - d.z;
-        return Vector3::new(0.7, 0.6, 1.).scale(a1.powf(4.));
+        let a1 = 1. - origin.z;
+        return Vec3::new(0.7, 0.6, 1.).scale(a1.powf(4.));
     }
 
-    let mut hit_point = o.add(d.scale(distance));
+    let mut hit_point = direction.add(origin.scale(distance));
 
-    let light_direction = Vector3::new(9. + random(), 9. + random(), 16.)
+    let light_direction = Vec3::new(9. + random(), 9. + random(), 16.)
         .add(hit_point.scale(-1.))
-        .normalize();
+        .norm();
 
-    let reflected_ray = d.add(surface_normal.scale(surface_normal.dot_product(d) * -2.));
+    let reflected_ray = origin.add(surface_normal.scale(surface_normal.dot(origin) * -2.));
 
-    let mut brightness = light_direction.dot_product(surface_normal);
+    let mut brightness = light_direction.dot(surface_normal);
 
     if brightness < 0.
         || trace(
@@ -147,23 +144,23 @@ fn sample_pixel_color(o: Vector3, d: Vector3) -> Vector3 {
     if material & 1 != 0 {
         hit_point = hit_point.scale(0.2);
 
-        let valor = hit_point.x.ceil() + hit_point.y.ceil();
+        let a1 = hit_point.x.ceil() + hit_point.y.ceil();
 
-        let valor_secreto = if valor as i32 & 1 != 0 {
-            Vector3::new(3., 1., 1.)
+        let color = if a1 as i32 & 1 != 0 {
+            Vec3::new(3., 1., 1.)
         } else {
-            Vector3::new(3., 3., 3.)
+            Vec3::new(3., 3., 3.)
         };
 
-        return valor_secreto.scale(brightness * 0.2 + 0.1);
+        return color.scale(brightness * 0.2 + 0.1);
     }
 
-    let contribution = if brightness > 0. {
-        light_direction.dot_product(reflected_ray).powf(99.)
+    let color_contribution = if brightness > 0. {
+        light_direction.dot(reflected_ray).powf(99.)
     } else {
         0.
     };
 
-    Vector3::new(contribution, contribution, contribution)
+    Vec3::new(color_contribution, color_contribution, color_contribution)
         .add(sample_pixel_color(hit_point, reflected_ray).scale(0.5))
 }
